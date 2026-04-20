5ead4bb92a4960ddb763279ca8c0dbd9b3fdbce3
PavanBhat007 <pvn.24.offi@gmail.com>
2026-04-20 21:27:01 +0530

    feat: integration of story generator in story endpoint

--- backend/core/story_generator.py
+++ backend/core/story_generator.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from core.config import settings
+from dotenv import load_dotenv
 
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
@@ -8,6 +8,8 @@ from langchain_core.output_parsers import PydanticOutputParser
 from core.prompts import STORY_PROMPT
 from core.models import StoryLLMResponse, StoryNodeLLM
 from models.story import Story, StoryNode
+
+load_dotenv()
 
 
 class StoryGenerator:
